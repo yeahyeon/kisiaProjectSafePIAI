ba96c17f0401cbdad56c920acfca55dc64417bf1
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-10-15 06:19:33 +0900

    Update forms.py

--- aiprojects/safepiai/protectphoto/forms.py
+++ aiprojects/safepiai/protectphoto/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 from .models import Photo
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
 
 class PhotoForm(forms.ModelForm):
+    images = MultipleFileField(widget=MultipleFileInput(attrs={'multiple': True}))
+    
     class Meta:
         model = Photo
-        fields = ['image']
+        fields = ['images']
