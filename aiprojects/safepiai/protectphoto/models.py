b3e3e2944be6b0cdedefe868262c308ef03dde27
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-10-15 06:19:49 +0900

    Update models.py

--- aiprojects/safepiai/protectphoto/models.py
+++ aiprojects/safepiai/protectphoto/models.py
@@ -1,17 +1,8 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 import os
 
 # Create your models here.
-#파일을 업로드하면, File object로 인식된다.
+#파일을 업로드하면, Image object로 인식된다.
+
 class Photo(models.Model):
-    image = models.FileField(null=True, blank=True, upload_to='uploads/')
-
-    class Meta:
-        app_label = 'protectphoto'
-
-    def save(self):
-        super().save()
+    image = models.ImageField(upload_to='uploads/')
