8fc98bccf89f378bc12f0a5f1e4d8015883ac0b1
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-11-05 10:37:52 +0900

    Update views.py

--- aiprojects/safepiai/protectphoto/views.py
+++ aiprojects/safepiai/protectphoto/views.py
@@ -26,22 +26,30 @@ def image_list(request):
     photos = Photo.objects.all()
     return render(request, 'image_list.html', {'photos': photos})
 
-def download_and_delete_image(request, pk):
+def download_and_delete_image(request):
 
-    photo_instance = get_object_or_404(Photo, pk=pk)
+     # 모든 이미지를 가져옵니다. 이 예제에서는 모든 이미지를 가져오지만 필요에 따라 필터링할 수 있습니다.
+    all_images = Photo.objects.all()
 
-    # 이미지 다운로드 응답
-    image_path = photo_instance.image.path
-    
-    with open(image_path, 'rb') as image_file:
-        response = HttpResponse(image_file.read(), content_type='image/jpeg')
-        response['Content-Disposition'] = f'attachment; filename="{photo_instance.image.name}"'
-    
-    if os.path.exists(image_path):
-        # 이미지 파일 삭제
-        os.remove(image_path)
+    # 다운로드 및 삭제된 이미지 수를 추적하는 변수를 초기화합니다.
+    downloaded_images = 0
 
-        # 이미지 모델 삭제
-        photo_instance.delete()
-    
-    return response
+    for photo_instance in all_images:
+        # 이미지 파일 경로를 가져옵니다.
+        image_path = photo_instance.image.path
+
+        with open(image_path, 'rb') as image_file:
+            response = HttpResponse(image_file.read(), content_type='image/jpeg')
+            response['Content-Disposition'] = f'attachment; filename="{photo_instance.image.name}"'
+
+        if os.path.exists(image_path):
+            # 이미지 파일 삭제
+            os.remove(image_path)
+
+            # 이미지 모델 삭제
+            photo_instance.delete()
+
+            downloaded_images += 1
+
+    # 모든 이미지를 처리한 후 응답을 반환합니다.
+    return HttpResponse(f'Downloaded and deleted {downloaded_images} images.')
